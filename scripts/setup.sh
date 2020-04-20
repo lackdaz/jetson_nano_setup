2a4d1130493acc7214a3bb59a43d646ef274207e
Seth Loh <lackdaz@gmail.com>
2020-04-21 01:34:15 +0800

    changed grep

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,7 +11,7 @@ fi
 if [ ! -f ~/.bash_aliases ]; then
     printf "\033[33;1mCreating .bash_aliases\033[0m\n"
     wget -P ~ https://raw.githubusercontent.com/lackdaz/jetson_nano_setup/master/scripts/.bash_aliases
-    if grep ".bash_aliases" $BASHRC > /dev/null; then
+    if grep -q ".bash_aliases" $BASHRC; then
         printf "\033[34;1mAlready exists in $BASHRC. Skipping step...\033[0m\n"
     else
         curl https://raw.githubusercontent.com/lackdaz/jetson_nano_setup/master/scripts/bash_aliases.sh >> $BASHRC
@@ -22,7 +22,7 @@ fi
 if [ ! -f ~/.bash/.bash_prompt.sh ]; then
     printf "\033[33;1mCreating .bash_aliases\033[0m\n"
     wget -P .bash https://raw.githubusercontent.com/lackdaz/jetson_nano_setup/master/scripts/.bash_prompt.sh
-    if grep ".bash_prompt" $BASHRC > /dev/null; then
+    if grep -q ".bash_prompt" $BASHRC; then
         printf "\033[34;1mAlready exists in $BASHRC. Skipping step...\033[0m\n"
     else
         curl https://raw.githubusercontent.com/lackdaz/jetson_nano_setup/master/scripts/bash_prompt.sh >> $BASHRC
@@ -32,7 +32,7 @@ fi
 
 if [ ! -f ~/.bash/.git-completion.sh ]; then
     wget -P .bash https://raw.githubusercontent.com/lackdaz/jetson_nano_setup/master/scripts/.git-completion.bash
-    if grep ".git-completion" $BASHRC > /dev/null; then
+    if grep -q ".git-completion" $BASHRC; then
         printf "\033[34;1mAlready exists in $BASHRC. Skipping step...\033[0m\n"
     else
         curl https://raw.githubusercontent.com/lackdaz/jetson_nano_setup/master/scripts/git-completion.sh >> $BASHRC
