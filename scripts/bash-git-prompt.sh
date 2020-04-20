86c10163cb3f7b1e1eaa697101a61caa2ae11ef4
Seth Loh <lackdaz@gmail.com>
2020-04-21 00:28:05 +0800

    more changes

--- scripts/bash-git-prompt.sh
+++ scripts/bash-git-prompt.sh
@@ -8,3 +8,5 @@ fi
 if [ -f ~/.testrc ]; then
     curl -K https://raw.githubusercontent.com/lackdaz/jetson_nano_setup/master/scripts/bashrc-append.sh >> ~/.testrc
 fi
+
+printf "\033[32;1mAppend Complete!\033[0m\n"
