b84b2a706f382b41ddbb8fb2c204a7ad659c34b0
seth <haloandwings@gmail.com>
2020-04-21 12:29:07 +0800

    working single bash script, before refactor

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 DIR=~/.bash
-BASHRC=~/.testrc
+BASHRC=~/testrc
 # Make directory
-if [ ! -f $DIR ]; then
+if [ ! -d $DIR ]; then
     mkdir ~/.bash
     printf "\033[32;1mCreating $DIR\033[0m\n"
 fi
 
+printf "\033[33;1mChecking for .bash_aliases\033[0m\n"
 if [ ! -f ~/.bash_aliases ]; then
+    printf "\033[34;1mNot Found!\033[0m\n"
     printf "\033[33;1mCreating .bash_aliases\033[0m\n"
     wget -P ~ https://raw.githubusercontent.com/lackdaz/jetson_nano_setup/master/scripts/.bash_aliases
-    
-    if ! grep -Fx ".bash_aliases" $BASHRX >/dev/null 2>/dev/null; then
-        printf "\033[33;1mUpdating $BASHRC...\033[0m\n"
-        echo >> $BASHRC # add newline
-        curl https://raw.githubusercontent.com/lackdaz/jetson_nano_setup/master/scripts/bash_aliases.sh >> $BASHRC
-        printf "\033[32;1mComplete!\033[0m\n"
-    else
-        printf "\033[34;1mAlready exists in $BASHRC. Skipping step...\033[0m\n"
-    fi
+else
+    printf "\033[34;1mFile exists. Skipping step...\033[0m\n"
+fi
+
+if ! grep ".bash_aliases" $BASHRC >/dev/null 2>/dev/null; then
+    printf "\033[33;1mUpdating $BASHRC...\033[0m\n"
+    echo >> $BASHRC # add newline
+    curl https://raw.githubusercontent.com/lackdaz/jetson_nano_setup/master/scripts/bash_aliases.sh >> $BASHRC
+    printf "\033[32;1mComplete!\033[0m\n"
+else
+    printf "\033[34;1mAlready exists in $BASHRC. Skipping step...\033[0m\n"
 fi
 
 # if [ ! -f ~/.bash/.bash_prompt.sh ]; then
@@ -47,4 +51,4 @@ if [ -f ~/.testrc ]; then
     curl https://raw.githubusercontent.com/lackdaz/jetson_nano_setup/master/scripts/bashrc-append.sh >> ~/.testrc
 fi
 
-printf "\033[32;1mAppend Complete!\033[0m\n"
+printf "\033[32;1mInstall Complete!\033[0m\n"
