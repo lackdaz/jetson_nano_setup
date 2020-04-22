3c8f3d9a484afbcfb6f0d1fa3789191651148eef
seth <haloandwings@gmail.com>
2020-04-22 18:05:12 +0800

    hdmi script working

--- scripts/set_hdmi_audio.sh
+++ scripts/set_hdmi_audio.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-BASHRC=~/testrc # for jetson nano
-APPEND='pactl set-default-sink  0'
+BASHRC=~/.bashrc # for jetson nano
+APPEND='pactl set-default-sink 0'
 
 printf "\033[33;1mChecking for previous entries in $BASHRC...\033[0m\n"
 
 if ! grep -qF "pactl set-default-sink 0" $BASHRC >/dev/null 2>/dev/null; then
     printf "\033[33;1mUpdating $BASHRC...\033[0m\n"
     echo >> $BASHRC # add newline
-    curl $APPEND >> $BASHRC
-    printf "\033[32;1mComplete!\033[0m\n"
+    echo $APPEND >> $BASHRC
+    echo $APPEND
+    printf "\033[32;1mAudio output is now set to HDMI.\033[0m\n"
 else
-    printf "\033[34;1mAlready exists in $BASHRC. Skipping step...\033[0m\n"
+    printf "\033[34;1mAlready exists in $BASHRC...\033[0m\n"
 fi
-printf "\033[32;1mAudio output is now set to HDMI.\033[0m\n"
